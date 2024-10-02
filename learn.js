9c7d694bb6b8cf6ffe63ac2cbf50750717eade50
khemraj saud <107249077+Khemrajsaud@users.noreply.github.com>
2024-10-02 16:43:29 +0545

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,14 @@
+// Const = a variable that can't changed
+let pi = 3.14;
+let radius;
+let circumference;
+
+pi = 345.87;
+
+radius = window.prompt('enter the radius of a circle');
+radius = Number(radius);
+
+circumference = 2*pi*radius;
+
+ console.log(circumference);
+ 
